2888e46e30dd9cc9cf6ae21343deb72f5f67660e
Ken Hilton <ken@skule.ca>
2026-02-18 15:01:49 -0500

    django.conf.urls.url was removed

--- skule_vote/skule_vote/urls.py
+++ skule_vote/skule_vote/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -37,7 +36,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
     path("vote/", CookieView.as_view(), name="cookie"),
-    url(
+    re_path(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
